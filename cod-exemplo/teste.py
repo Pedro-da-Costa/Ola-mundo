235f7844994a09d05676838b76d2db0fc4828dd3
Pedro Cardoso <pedrocardosolivestream@gmail.com>
2024-03-15 18:52:49 -0300

    Criando Código Python
    Código simples Python

---
+++ cod-exemplo/teste.py
@@ -0,0 +1,9 @@
+nota1 = (int(input("Digite a sua nota: ")))
+nota2 = (int(input("Digite a sua segunda nota: ")))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print("Você foi aprovado")
+else:
+    print("Você foi reprovado")
