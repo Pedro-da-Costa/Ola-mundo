63c2507e2bbbd1fcbaf2186f3f5b702a1f85f1fb
Pedro Cardoso <pedrocardosolivestream@gmail.com>
2024-03-15 18:56:56 -0300

    Adicionei novas funcionalidades

--- cod-exemplo/teste.py
+++ cod-exemplo/teste.py
@@ -1,9 +1,12 @@
-nota1 = (int(input("Digite a sua nota: ")))
-nota2 = (int(input("Digite a sua segunda nota: ")))
+nota1 = (float(input("Digite a sua nota: ")))
+nota2 = (float(input("Digite a sua segunda nota: ")))
 
 media = (nota1 + nota2) / 2
+print(f'Sua média foi {media:.2f}')
 
-if media >= 7:
+if media >= 5 and media < 7:
+    print("Você está de recuperação")
+elif media >= 7:
     print("Você foi aprovado")
 else:
     print("Você foi reprovado")
